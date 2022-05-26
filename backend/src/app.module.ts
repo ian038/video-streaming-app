32ebeb5cbab39e99b410af3a3df2970648d614fd
ian038 <ianphua3@gmail.com>
2022-05-26 10:27:03 -0400

    Updated app module ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,6 +8,12 @@ import { join } from 'path';
 import { MulterModule } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { v4 as uuidv4 } from 'uuid';
+import { VideoController } from './controller/video.controller';
+import { VideoService } from './service/video.service';
+import { UserService } from './service/user.service';
+import { UserController } from './controller/user.controller';
+import { Video, VideoSchema } from './model/video.schema';
+import { User, UserSchema } from './model/user.schema';
 require('dotenv').config()
 
 const secret = process.env.jwtSecret
@@ -15,6 +21,8 @@ const secret = process.env.jwtSecret
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([{ name: Video.name, schema: VideoSchema }]),
     MulterModule.register({
       storage: diskStorage({
         destination: './public',
@@ -32,7 +40,7 @@ const secret = process.env.jwtSecret
       rootPath: join(__dirname, '..', 'public')
     })
   ],
-  controllers: [AppController],
-  providers: [AppService]
+  controllers: [AppController, VideoController, UserController],
+  providers: [AppService, VideoService, UserService]
 })
 export class AppModule { }
